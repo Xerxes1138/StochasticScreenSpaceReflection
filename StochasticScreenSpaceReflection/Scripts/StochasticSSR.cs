9b5db20b5fd9cfb80fc96b45d02aefeb2c3a39fe
Charles Greivelding Thomas <charles.greivelding@gmail.com>
2016-08-04 13:16:30 +0200

    Fixed depth/motion vector check

--- StochasticScreenSpaceReflection/Scripts/StochasticSSR.cs
+++ StochasticScreenSpaceReflection/Scripts/StochasticSSR.cs
@@ -137,11 +137,10 @@ namespace cCharkes
 
         RenderBuffer[] renderBuffer = new RenderBuffer[2];
 
-        void OnEnable()
+        void Awake()
         {
             m_camera = GetComponent<Camera>();
-            m_camera.depthTextureMode = DepthTextureMode.Depth;
-            m_camera.depthTextureMode = DepthTextureMode.MotionVectors;
+            m_camera.depthTextureMode |= DepthTextureMode.Depth | DepthTextureMode.MotionVectors;
         }
 
         static Material m_rendererMaterial = null;
